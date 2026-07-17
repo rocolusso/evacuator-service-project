625180e570ec92ad95d02d9be9b9a3eb73e17a77
Rocolusso <rocolusso@icloud.com>
2026-07-17 06:10:50 +0300

    updated robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,3 +1,20 @@
+// import type { MetadataRoute } from 'next';
+//
+// export default function robots(): MetadataRoute.Robots {
+//   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
+//
+//   return {
+//     rules: [
+//       {
+//         userAgent: '*',
+//         allow: '/',
+//         disallow: ['/api/'],
+//       },
+//     ],
+//     sitemap: `${baseUrl}/sitemap.xml`,
+//   };
+// }
+
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
@@ -7,10 +24,10 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
-        disallow: ['/api/'],
+        disallow: ['/'],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
+
