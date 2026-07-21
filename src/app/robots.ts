12a953757abc2f9db9ea3162787e4d617e3b19cf
Rocolusso <rocolusso@icloud.com>
2026-07-21 11:36:33 +0300

    updated robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -24,7 +24,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        disallow: ['/'],
+        allow: ['/'],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
